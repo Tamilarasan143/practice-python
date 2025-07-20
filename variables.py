a744fcbd26f9b577f622b6864882fe1613d838b2
tamilarasan elumalai <tamilarasan@lynexsoft.com>
2025-07-20 21:10:37 +0530

    learning variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+# There is no need of any kind keywords like const , var ,
+# let to present variables in python just use name and = to there values
+x = 10
+a = False
+b = 4.99
+c = "Hi"
+
+print(x)
+name = "tamil"
+name_length = len(name)
+print(name_length)
+print(name[0])
+print(name[0:name_length])
+
+# escape character
+# \" add "" in center of string
+# \' add ' in center of string
+# \\ add \ in center of string
+# \n to break the string or new line string
+
+# Formatted String
+escape_name = f"tamil length : {name_length}"
+print(escape_name)

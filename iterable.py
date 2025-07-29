75492ed425a8b48b9029a3bcc75b2048018f4b33
tamilarasan elumalai <tamilarasan@lynexsoft.com>
2025-07-29 11:12:24 +0530

    today learned types of iterable with loops

---
+++ iterable.py
@@ -0,0 +1,8 @@
+# Range not only the iterable in for loop
+# There are so many objects make iterable with for loop like string , list
+
+for t in "Tamil":
+    print(t)
+print("<-----X------>")
+for l in [1, 2, 3, 4, 5, 6]:
+    print(l)

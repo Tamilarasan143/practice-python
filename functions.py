b43bd70d6a5f89aa2ce84ef1bbeacf77327dd881
tamilarasan elumalai <tamilarasan@lynexsoft.com>
2025-07-31 12:59:59 +0530

    learned function and argument function

---
+++ functions.py
@@ -0,0 +1,15 @@
+# def means define
+# In python by default the function are return "None Object" until we return anything
+def greeting():
+    print("Hello World")
+
+
+greeting()
+
+
+# Argument function
+def addition(one, two):
+    return one + two
+
+
+print(addition(two=2, one=1))

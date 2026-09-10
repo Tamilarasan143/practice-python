6436123eba1bae0eb1f5660e7d52ab97d7ac8ed4
Tamilarasan Elumalai <tamilarasan1432000@gmail.com>
2026-09-10 10:53:53 +0530

    Practicing dictionary dsa

---
+++ DSA/Dictionary/dictionary.py
@@ -0,0 +1,64 @@
+numbers = [10, 5,5, 20, 5,]
+
+def count_frequency(numbers: list[int]) -> dict[int, int]:
+    frequency_map = {}
+    for number in numbers:
+        if number in frequency_map:
+            frequency_map[number] +=1
+        else:
+            frequency_map[number] = 1
+    return frequency_map
+
+
+def find_most_frequent(numbers: list[int]) -> int:
+    frequency_map = {}
+    most_frequent_count = 0
+    most_frequent_key = None
+    for number in numbers:
+        if number in frequency_map:
+            frequency_map[number] +=1
+        else:
+            frequency_map[number] = 1
+
+    for key , value in frequency_map.items():
+         if value > most_frequent_count:
+            most_frequent_count = value
+            most_frequent_key = key
+
+    return most_frequent_key    
+
+def find_first_non_repeating(numbers: list[int]) -> int:
+    frequency_map = {}
+
+    for number in numbers:
+        if number in frequency_map:
+            frequency_map[number] +=1
+        else:
+            frequency_map[number] = 1
+
+    for number in numbers:
+       if frequency_map[number] == 1:
+           return number
+def find_first_repeating(numbers: list[int]) -> int:
+    frequency_map = {}
+    for number in numbers:
+        if number in frequency_map:
+                return number
+        else:
+            frequency_map[number] = 1
+
+def two_sum(numbers: list[int], target: int) -> list[int]:
+     sum_map = {}
+     for index , number in enumerate(numbers):
+        needed =  target - number
+        if needed in sum_map:
+            return [sum_map[needed],index]
+        else:
+            sum_map[number] = index
+
+if __name__ == "__main__":
+  print("Count Frequency",count_frequency(numbers))
+  print("Most Frequency element",find_most_frequent(numbers))
+  print("first Non Frequency element",find_first_non_repeating(numbers))
+  print("fits Frequency element",find_first_repeating(numbers))
+  print("two sum",two_sum([7, 11, 15,2],9))
--- DSA/List/find-max-element.py
+++ DSA/List/find-max-element.py
@@ -1,5 +1,4 @@
-import enum
-numbers = [10, 20 , 20 ,10 , 5]      
+numbers = [10,10 ,20 , 20  , 5]      
 
 
 def find_max_element(numbers:list[int]):
@@ -47,8 +46,42 @@ def count_occurrences(numbers: list[int], target: int) -> int:
 
    return counter
 
+def find_first_duplicate(numbers: list[int]) -> int:
+    unique_element = set()
+    first_duplicate = None
+    for number in numbers:
+        if number in unique_element and first_duplicate is None:
+            first_duplicate = number
+        unique_element.add(number)
+    if first_duplicate is None:
+        raise ValueError("There is no duplicates found in this list")
+    return first_duplicate 
+
+def remove_duplicates(numbers: list[int]) -> list[int]:
+      unique_element = set()
+      unique_element_list = list()
+      for number in numbers:
+        if number not in unique_element:
+            unique_element_list.append(number)
+        unique_element.add(number)
+      return  unique_element_list
+      
+def find_missing_number(numbers: list[int]) -> int:
+    n = len(numbers) + 1
+
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(numbers)
+
+    return expected_sum - actual_sum
+            
+
+      
 if __name__ == "__main__":
- print(find_max_element(numbers))
- print(find_second_largest(numbers))
- print(find_min_element(numbers))
- print(count_occurrences(numbers,10))
+ print("Max Element",find_max_element(numbers))
+ print("Second largest Element",find_second_largest(numbers))
+ print("Min largest Element",find_min_element(numbers))
+ print("Target Element counter",count_occurrences(numbers,10))
+ print("Find first Element" , find_first_duplicate(numbers))
+ print("Remove duplicates and return unique",remove_duplicates(numbers))
+#  print("Missing patten Number",find_missing_number([1,2,3,5]))
+find_missing_number([1,2,3,5])
